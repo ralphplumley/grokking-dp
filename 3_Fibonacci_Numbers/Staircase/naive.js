90984f4ef7a13f9bc5bf922d37a139b2b5f41f57
Ralph Plumley <ralphplumley@gmail.com>
2021-07-16 10:30:48 -0400

    complete stairs naive

---
+++ 3_Fibonacci_Numbers/Staircase/naive.js
@@ -0,0 +1,14 @@
+const CountWays = function(n) {
+    if (n === 0 || n === 1) return 1
+    if (n === 2) return 2
+
+    const count1 = CountWays(n-1)
+    const count2 = CountWays(n-2)
+    const count3 = CountWays(n-3)
+
+    return count1 + count2 + count3
+};
+
+console.log(`Number of ways: ---> ${CountWays(3)}`);
+console.log(`Number of ways: ---> ${CountWays(4)}`);
+console.log(`Number of ways: ---> ${CountWays(5)}`);
